1b24d87e3c170b2323900f6090952e2d3db94a25
Travis B. Hartwell <nafai@travishartwell.net>
2015-06-10 19:31:49 -0600

    Add package for Travis Hartwell Mail

--- local-pkgs.nix
+++ local-pkgs.nix
@@ -1,5 +1,6 @@
 {
   nixpkgs.config.packageOverrides = pkgs: rec {
     gtkmenu = pkgs.haskellPackages.callPackage ../gtkmenu {};
+    travis-hartwell-mail = pkgs.callPackage ../local-apps/travis-hartwell-mail {};
   };
 }
