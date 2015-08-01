9244ea56aba3d9d23215cebc3eb63657e48d86f2
Travis B. Hartwell <nafai@travishartwell.net>
2015-07-31 20:37:13 -0600

    Make more robust.  Design to run from i3.

--- check-channel-update.sh
+++ check-channel-update.sh
@@ -5,24 +5,48 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+readonly UPDATE_MESSAGE="New channel update!"
+readonly CHECK_FREQUENCY="1h";
+
 # Check out the channels repo with the channel you are running, like:
-# git clone --depth=1 --branch=nixos-unstable git@github.com:NixOS/nixpkgs-channels.git
+# git clone --depth=1 --branch=nixos-unstable \
+#     git@github.com:NixOS/nixpkgs-channels.git
 readonly CHANNELS_DIR="${HOME}/Projects/nix/nixpkgs-channels"
 readonly LOCAL_VERSION=$(nixos-version | sed 's/.*\.\([^ ]*\) .*/\1/')
 
-cd "${CHANNELS_DIR}"
-git pull -q
+check_for_update() {
+    cd "${CHANNELS_DIR}"
+    readonly local FULL_LOCAL_CURRENT_SHA1=$(git rev-parse "${LOCAL_VERSION}")
 
-readonly FULL_LOCAL_CURRENT_SHA1=$(git rev-parse "${LOCAL_VERSION}")
-readonly CHANNEL_CURRENT_SHA1=$(git rev-parse HEAD)
+    git pull -q
+    readonly local CHANNEL_CURRENT_SHA1=$(git rev-parse HEAD)
 
-readonly UPDATE_MESSAGE="New channel update!"
-
-if [ "${FULL_LOCAL_CURRENT_SHA1}" == "${CHANNEL_CURRENT_SHA1}" ]; then
-    # If we are running interactively or from where an X display isn't available
-    if [ -n "$(tty)" ] || [ ! -v DISPLAY ] ; then
-        echo "${UPDATE_MESSAGE}"
-    else
-        yad --notification --image="software-update-available" --text="${UPDATE_MESSAGE}"
+    if [ "${FULL_LOCAL_CURRENT_SHA1}" != "${CHANNEL_CURRENT_SHA1}" ]; then
+        # If we are running interactively or
+        # from where an X display isn't available
+        if [ -n "$(tty)" ] || [ ! -v DISPLAY ] ; then
+            echo "${UPDATE_MESSAGE}"
+        else
+            yad --notification \
+                --image="software-update-available" \
+                --text="${UPDATE_MESSAGE}"
+        fi
     fi
-fi
+}
+
+main () {
+    [[ $# -gt 0 && "$1" == "--loop" ]] &&
+        readonly local loop_infinitely=true || loop_infinitely=false
+
+    while true; do
+        check_for_update
+
+        if [ "$loop_infinitely" = "true" ]; then
+            sleep "$CHECK_FREQUENCY"
+        else
+            break
+        fi
+    done
+}
+
+main "$@"
