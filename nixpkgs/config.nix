c13819239b23422b766e49c6a821544157528703
Travis B. Hartwell <nafai@travishartwell.net>
2015-10-22 20:53:49 -0600

    Make sure cache and chroot configuration is available to the user environment.

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -1,6 +1,11 @@
 {
   allowUnfree = true;
 
+  useChroot = true;
+  binaryCaches =  [ "https://cache.nixos.org/" ];
+  trustedBinaryCaches = [ "https://cache.nixos.org/" "https://hydra.nixos.org" ];
+  binaryCachePublicKeys = [ "hydra.nixos.org-1:CNHJZBh9K4tP3EKF6FkkgeVYsS3ohTl+oS0Qa8bezVs=" ];
+
   firefox = {
     enableGoogleTalkPlugin = true;
     enableAdobeFlash = true;
